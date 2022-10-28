a7f4310520278f001e06ee29e0b6e792b1c02624
kate <chimmi10kate@mail.ru>
2022-10-28 11:14:25 +0300

    deleted console logs

--- server/common/index.js
+++ server/common/index.js
@@ -3,11 +3,9 @@ import axios from 'axios'
 import { readFile } from 'fs/promises'
 
 export const getProductsFunc = () => {
-  return (
-    readFile(`${__dirname}/../data/data.json`, 'utf-8')
-      .then((data) => JSON.parse(data))
-      .catch(() => [])
-  )
+  return readFile(`${__dirname}/../data/data.json`, 'utf-8')
+    .then((data) => JSON.parse(data))
+    .catch(() => [])
 }
 
 export const sortProductsList = (arrayOfProducts, sortType, direction) => {
@@ -38,7 +36,7 @@ function rateChecker() {
   const msAtHour = 1000 * 60 * 60 // 1 Hour
   let currency = {}
   return {
-    checkDate: (dateMs = 0) => (ratesRequestDate + msAtHour) <= dateMs,
+    checkDate: (dateMs = 0) => ratesRequestDate + msAtHour <= dateMs,
     setRateDate: (dateMs = 0) => {
       ratesRequestDate = dateMs
     },
@@ -54,15 +52,14 @@ const myRates = rateChecker()
 export const getRates = async () => {
   const url = 'https://api.exchangerate.host/latest?base=USD&symbols=USD,EUR,CAD'
   const mockRates = {
-    "CAD": 1.3,
-    "EUR": 0.9,
-    "USD": 1
+    CAD: 1.3,
+    EUR: 0.9,
+    USD: 1
   }
 
   const date = +new Date()
 
   if (myRates.checkDate(date)) {
-    console.log('Wait data from exchange API...')
     await axios(url)
       .then(({ data }) => data.rates)
       .then((cur) => myRates.setCurrency(cur))
